13cf0abba77cb6c8dc681956e89c6734c2ea4c37
c0a2413053 <c0a2413053@edu.teu.ac.jp>
2025-04-15 16:02:52 +0900

    練習9:背景ループ処理

--- pygame_image.py
+++ pygame_image.py
@@ -15,16 +15,21 @@ def main():
     pc_img = pg.image.load("fig/3.png")
     pc_img = pg.transform.flip(pc_img, 1, 0)
     tmr = 0
+    x = tmr
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img2, [-tmr+1600, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(pc_img, [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        x += 1        
         clock.tick(200)
+        if x >= 3200:
+            x = 0
 
 
 if __name__ == "__main__":
