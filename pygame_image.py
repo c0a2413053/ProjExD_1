4cc1639d7abd6fbb0620b6cd13b87af041379e4a
c0a2413053 <c0a2413053@edu.teu.ac.jp>
2025-04-15 16:17:19 +0900

    練習10:こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img2, 1, 0)
     pc_img = pg.image.load("fig/3.png")
     pc_img = pg.transform.flip(pc_img, 1, 0)
+    pc_rct = pc_img.get_rect()
+    pc_rct.center = 300, 200
     tmr = 0
     x = tmr
     while True:
@@ -23,7 +25,17 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(pc_img, [300, 200])
+
+        key_lst = pg.key.get_pressed()  
+        if key_lst[pg.K_UP]:
+            pc_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            pc_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            pc_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            pc_rct.move_ip(+1, 0)            
+        screen.blit(pc_img, pc_rct)
         pg.display.update()
         tmr += 1
         x += 1        
